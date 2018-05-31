2b3853c778839ee0d812db6962d7024a8a422d7b
naanayahernandez <naanayahernandez@gmail.com>
2018-05-30 23:47:33 -0500

    day 10 and 11

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,34 @@
+//Get our elements
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+//Build out functions
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    console.log('skipping');
+}
+
+//Hook up the even listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
